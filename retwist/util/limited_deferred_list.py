c46c99737bccef530939bb5f675eef02659d0afb
Steffen Wenz <swenz@trustyou.net>
2017-05-28 07:26:13 +0200

    Added LimitedDeferredList

---
+++ retwist/util/limited_deferred_list.py
@@ -0,0 +1,73 @@
+import collections
+
+import twisted.internet.defer
+
+
+class LimitedDeferredList(twisted.internet.defer.Deferred):
+    """
+    Run a list of Deferreds in parallel, much like DeferredList, but limit the number of concurrently executed
+    Deferreds.
+
+    Useful if you need to run 100 things in parallel, but don't want to run more than 10 at once, e.g. to limit access
+    to system resources.
+
+    Unlike DeferredList, you can't pass a list of Deferreds to this class, since they would be running already. Instead,
+    pass a list of factory methods that make the Deferred on demand.
+
+    If any single one of the Deferreds fails, the overall LimitedDeferredList will errback with the inidividual
+    Deferred's failure object.
+    """
+
+    def __init__(self, deferred_factories, max_concurrent):
+        """
+        :param deferred_factories: A list of callables which, when invoked without arguments, return a Deferred.
+        :param max_concurrent: Run no more than max_concurrent Deferreds at the same time.
+        """
+        twisted.internet.defer.Deferred.__init__(self)
+
+        # Index the Deferreds so we can order the results when they callback
+        indexed_factories = enumerate(deferred_factories)
+        self.deferred_factories = collections.deque(indexed_factories)
+
+        self.results = [None] * len(self.deferred_factories)
+        self.counter = 0
+
+        # Start up max_concurrent Deferreds! Later, we start a new Deferred for every one that callbacks. So we don't
+        # even need to store max_concurrent
+        for _ in range(max_concurrent):
+            self.__schedule_deferred()
+
+    def __deferred_callback(self, result, index):
+        """
+        Callback for every one of the scheduled Deferreds.
+        :param result: Result of the Deferred
+        :param index: Index in overall result list.
+        """
+        self.counter -= 1
+        self.results[index] = result
+        # If there are no other Deferreds running and no more to be scheduled, we're all done. Resolve with the results
+        # list
+        if not self.__schedule_deferred() and self.counter == 0:
+            self.callback(self.results)
+
+    def __deferred_errback(self, failure):
+        """
+        Errback for every one of the scheduled Deferreds. Make the whole LimitedDeferredList fail.
+        :param failure: Failure object
+        """
+        # TODO Should we cancel still running Deferreds?
+        self.errback(failure)
+
+    def __schedule_deferred(self):
+        """
+        Schedule the next available Deferred.
+        :return: True if a Deferred was scheduled, False if not (i.e. we're done with our work)
+        """
+        if self.deferred_factories:
+            self.counter += 1
+            index, deferred_factory = self.deferred_factories.pop()
+            deferred = deferred_factory()
+            deferred.addCallback(self.__deferred_callback, index)
+            deferred.addErrback(self.__deferred_errback)
+            return True
+        return False
---
+++ tests/test_limited_deferred_list.py
@@ -0,0 +1,99 @@
+import collections
+import random
+
+import pytest
+import twisted.internet.defer
+import twisted.internet.reactor
+
+from retwist.util.limited_deferred_list import LimitedDeferredList
+
+
+@pytest.inlineCallbacks
+def test_limited_deferred_list():
+    """
+    Test that no more than max_concurrent Deferreds are run at the same time, and that results are returned in correct
+    order.
+    """
+
+    max_concurrent = 2
+    counter = collections.Counter()
+
+    def make_deferred_factory(index):
+        """
+        Return a Deferred factory for testing purposes. Makes a Deferred which resolves with "Deferred {index} done"
+        after a random time in [0, 10ms].
+        :return: Function which, when invoked, returns a Deferred
+        """
+        def deferred_factory():
+            counter["running"] += 1
+            deferred = twisted.internet.defer.Deferred()
+
+            def resolve_deferred():
+                # Check that, at no point, there are more than max_concurrent Deferreds running
+                assert counter["running"] <= max_concurrent
+                deferred.callback("Deferred {} done".format(index))
+                counter["running"] -= 1
+
+            # Wait between 0 and 10ms before resolving. Do this to ensure Deferreds are run in shuffled order
+            delay = random.random() * 0.01
+            twisted.internet.reactor.callLater(delay, resolve_deferred)
+
+            return deferred
+        return deferred_factory
+
+    deferred_factories = [make_deferred_factory(index) for index in range(10)]
+    defs = LimitedDeferredList(deferred_factories, max_concurrent)
+
+    results = yield defs
+
+    expected_results = ["Deferred {} done".format(index) for index in range(10)]
+    assert expected_results == results
+
+
+@pytest.inlineCallbacks
+def test_synchronous_deferreds():
+    """
+    Test that deferreds that callback synchronously don't mess up our control flow.
+    """
+
+    def deferred_factory():
+        deferred = twisted.internet.defer.Deferred()
+        deferred.callback("done")
+        return deferred
+
+    # While we're at it, test that we accept an iterable for deferred_factories ...
+    deferred_factories = (deferred_factory for _ in range(2))
+    # ... and that max_concurrent can be larger than the number of Deferreds you schedule:
+    defs = LimitedDeferredList(deferred_factories, 10)
+
+    results = yield defs
+
+    assert ["done", "done"] == results
+
+
+@pytest.inlineCallbacks
+def test_error_handling():
+
+    counter = collections.Counter()
+
+    def deferred_factory():
+        deferred = twisted.internet.defer.Deferred()
+        deferred.errback(RuntimeError("The Internet is broken"))
+        return deferred
+
+    deferred_factories = [deferred_factory]
+    defs = LimitedDeferredList(deferred_factories, 2)
+
+    def callback(result):
+        # This should not run
+        assert False
+
+    def errback(failure):
+        # This should run exactly once
+        counter["errback"] += 1
+
+    defs.addCallbacks(callback, errback)
+
+    yield defs
+
+    assert counter["errback"] == 1
