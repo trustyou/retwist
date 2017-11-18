b3469be11426124388461c2197249406de4a8449
Steffen Wenz <swenz@trustyou.net>
2017-11-18 18:07:24 +0100

    Release v0.2

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 import setuptools
 
-version = "0.1"
+version = "0.2"
 
 setuptools.setup(
     name="retwist",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
     description="Write JSON REST APIs in the Twisted framework",
     author="TrustYou",
     author_email="development@trustyou.com",
     version=version,
-    download_url="https://github.com/trustyou/retwist/archive/{}.tar.gz".format(version),
+    download_url="https://github.com/trustyou/retwist/archive/v{}.tar.gz".format(version),
     url="https://github.com/trustyou/retwist",
     install_requires=[
         "twisted",
