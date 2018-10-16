d9438acddc6be63d38c9fbced1d6afe610da496f
jorge jardines <jorge.jardines@trustyou.net>
2018-10-16 14:28:03 +0200

    Add classifiers

--- setup.py
+++ setup.py
@@ -15,6 +15,16 @@ setuptools.setup(
         "twisted==18.9.0",
         "typing;python_version<'3.5'"
     ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
     extras_require={
         "sentry": ["raven"]
     }
